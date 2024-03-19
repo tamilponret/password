f4e7296b65321195c632c8687a79242580f779c6
tamilponret <146553574+tamilponret@users.noreply.github.com>
2024-03-19 21:55:07 +0530

    Add files via upload

---
+++ tamilazhagi.py
@@ -0,0 +1,18 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x*y):
+    return x*y
+def divide(x,y):
+    if y == 0:
+        return "Erroe"!Division by Zero."
+     else:
+        return x/y
+    print("select operation :")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply ")
+    print("4. Division")
+    
+        
